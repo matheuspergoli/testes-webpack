b42053f3e813c8aa512171bc60cdbfdcdf2c4360
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-05-25 13:51:54 -0300

    Plugins

--- plugins/webpack.config.js
+++ plugins/webpack.config.js
@@ -28,7 +28,7 @@ module.exports = {
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: 'style.css'
+      filename: '[name][contenthash].css'
     }),
     new webpack.DefinePlugin({
       VERSION: JSON.stringify('1.0.2'),
