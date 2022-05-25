b3f1b9da27b79556e46b8544819ed9ff671d644a
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-05-25 03:10:13 -0300

    Plugins

--- plugins/src/script.js
+++ plugins/src/script.js
@@ -2,3 +2,7 @@ import Heading from './components/heading/heading.js'
 
 const heading = new Heading()
 heading.create('Página Principal')
+
+console.log(VERSION)
+console.log(PORT)
+console.log(process.env.API_KEY)
--- plugins/webpack.config.js
+++ plugins/webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const TerserPlugin = require('terser-webpack-plugin')
+const webpack = require('webpack')
+const DotenvPlugin = require('dotenv-webpack')
 
 module.exports = {
   entry: "./src/script.js",
@@ -26,6 +28,11 @@ module.exports = {
   plugins: [
     new MiniCssExtractPlugin({
       filename: 'style.css'
-    })
+    }),
+    new webpack.DefinePlugin({
+      VERSION: JSON.stringify('1.0.2'),
+      PORT: JSON.stringify('8080')
+    }),
+    new DotenvPlugin()
   ]
 }
